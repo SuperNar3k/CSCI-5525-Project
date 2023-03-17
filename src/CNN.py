bf08f8fd92547f2c670f74a74743defd202b1cda
Alex <zhux0533@umn.edu>
2023-03-17 12:51:02 -0500

    add skeleton

--- src/CNN.py
+++ src/CNN.py
@@ -5,18 +5,19 @@
 # Using pytorch?
 
 class CNN:
-    def __init__(self):
+    def __init__(self, input_size, output_size, kernel_size, stride_size,
+                 max_pool_size, learning_rate, max_epochs):
         """Constructor, initialize class parameters and layers."""
         pass
     
-    def forward(self):
+    def forward(self, X):
         """Propagate forward"""
         pass
 
-    def fit(self):
+    def fit(self, train_loader, criterion, optimizer):
         """Train and fit"""
         pass
 
-    def predict(self, x):
+    def predict(self, test_loader, criterion):
         """Predict data"""
         pass
