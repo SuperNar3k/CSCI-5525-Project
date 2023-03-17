f39d06bdfbf81716618e2c2696e6193bb1d1a1dd
Alex <zhux0533@umn.edu>
2023-03-17 11:12:37 -0500

    initial commit

---
+++ src/CNN.py
@@ -0,0 +1,22 @@
+# CSCI 5525 - Group 11
+# An initial Convolutional Neural Network for simple image classification
+# using the HASYv2 dataset
+
+# Using pytorch?
+
+class CNN:
+    def __init__(self):
+        """Constructor, initialize class parameters and layers."""
+        pass
+    
+    def forward(self):
+        """Propagate forward"""
+        pass
+
+    def fit(self):
+        """Train and fit"""
+        pass
+
+    def predict(self, x):
+        """Predict data"""
+        pass
