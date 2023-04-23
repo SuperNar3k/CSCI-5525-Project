d3d0feaf8bb799b7118a67a90c7f4a6ad212561a
Alex <zhux0533@umn.edu>
2023-04-23 15:25:57 -0500

    refactor crohme data handling

--- src/data/crohme/CROHMEDataset.py
+++ src/data/crohme/CROHMEDataset.py
@@ -1,48 +1,47 @@
 # CSCI 5525 | Group 11
-# Written by Alex
 import matplotlib.pyplot as plt
 import os
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+from PIL import Image
 
-from data.crohme import CROHMEDataset as crohme
-from data.hasy import hasy_tools as hasy
+class CROHMEDatset(Dataset):
+    def __init__(self, image_folder=os.getcwd()+"\\src\\data\\crohme\\images", label_file=os.getcwd()+"\\src\\data\\crohme\\CROHME_math.txt", transform=transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])):
+        """
+        Make sure you unzip the files first.
 
-################################
-#        Load HASYv2(s)        #
-################################
+        Args:
+            - image_folder (str): path to folder of crohme images
+            - label_file (str): path to labels .txt
+            - transform: transform list of normalizing values for images
+        """
+        self.image_folder = image_folder
+        self.label_file = label_file
+        self.transform = transform
+        with open(label_file, 'r') as f:
+            self.labels = [line.strip() for line in f.readlines() if line != ""]
 
-# Be sure to unzip the .tar before running
-# help(hasy) # prints some helpful tools, somewhat deprecated?
-def load_hasy(csv_filepath=None):
-    """
-    Load HASYv2 data
-        - Note for the file path there are 10 folders (fold-1 through fold-10)
-          each which have train.csv and test.csv
-    """
-    if csv_filepath is None:
-        raise ValueError("Filepath to .csv is not defined")
+        self.image_files = sorted(os.listdir(image_folder))
+
+    def __len__(self):
+        return len(self.labels)
     
-    print(f"Loading HASYv2 data from: '{csv_filepath}'")
-    symbol_index = hasy.generate_index(csv_filepath)            # returns dict of labels
-    images,labels = hasy.load_images(csv_filepath,symbol_index) # images has size (index,y,x,depth)
-    print(f"Loaded {len(images)} images...")
-
-    return images, labels
-
-def plot_hasy_img(image):
-    # plot a sample image has size (32,32), b&w img (1s and 0s)
-    plt.imshow(hasy.thresholdize(image), cmap='gray')       # plot b&w image
-    plt.show()
-
-################################
-#         Load CROHME          #
-################################
-
-# Load CROHME LaTeX data
-## default batch sizes are 32
-## see details within CROHMEDataset.py
+    def __getitem__(self, idx):
+        filename = self.image_files[idx]
+        img_path = os.path.join(self.image_folder, filename)
+        img = Image.open(img_path).convert('RGB')
+        
+        if self.transform:
+            img = self.transform(img)
+        
+        label = self.labels[idx]
+        return img, label, filename
+    
 def get_CROHME_loaders(batch_size=32):
     dataset = CROHMEDatset()
 
@@ -63,7 +62,7 @@ def get_CROHME_loaders(batch_size=32):
 
     return train_loader, test_loader, val_loader
 
-def plot_crohme_img(img, std=[0.485, 0.456, 0.406], mean=[0.229, 0.224, 0.225]):
+def print_sample(img, std=[0.485, 0.456, 0.406], mean=[0.229, 0.224, 0.225]):
     plt.imshow(unnormalize(img, std, mean))
     plt.show()
 
@@ -85,7 +84,7 @@ if __name__ == "__main__":
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    dataset = crohme.CROHMEDatset(image_folder, label_file, transform=transform)
+    dataset = CROHMEDatset(image_folder, label_file, transform=transform)
 
     # Split the dataset into training, testing, and validation sets
     train_ratio, test_ratio, val_ratio = 0.7, 0.2, 0.1
@@ -104,10 +103,10 @@ if __name__ == "__main__":
 
     # Get a sample from the train_loader
     data_iter = iter(train_loader)
-    sample_images, sample_labels, _ = next(data_iter)
+    sample_images, sample_labels = next(data_iter)
 
     # Display the first image and its label
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
-    plot_crohme_img(sample_images[0], mean, std)
+    print_sample(sample_images[0], mean, std)
     print("Label:", sample_labels[0])
